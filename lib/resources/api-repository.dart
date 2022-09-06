0c142e3a27255b8e5d8907b6c651c9766c1534e8
nicolau <estevao_nicolau@hotmail.com>
2022-09-06 20:26:23 -0300

    :thread:

---
+++ lib/resources/api-repository.dart
@@ -0,0 +1,12 @@
+import '../model/covid-model.dart';
+import 'api-provider.dart';
+
+class ApiRepository {
+  final _provider = ApiProvider();
+
+  Future<CovidModel> fetchCovidList() {
+    return _provider.fetchCovidList();
+  }
+}
+
+class NetworkError extends Error {}
